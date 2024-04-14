f54a8413f3b17ee24d75024a6d45cde9216da506
Dri3xCz <Dri3xCz@gmail.com>
2024-04-15 00:16:52 +0200

    feat: added input handling

---
+++ InputHandeler.cs
@@ -0,0 +1,26 @@
+﻿namespace OsuMultiplayerLobbyFinder
+{
+    public class InputHandeler
+    {
+        public InputHandeler() { }
+
+        public int ReadInt()
+        {
+            while (true)
+            {
+                try
+                {
+                    return Convert.ToInt32(Console.ReadLine());
+                } catch
+                {
+                    Console.WriteLine("Incorrect value, try again!");
+                }
+            }
+        }
+
+        public string ReadString()
+        {
+            return Console.ReadLine() ?? "";
+        }
+    }
+}
