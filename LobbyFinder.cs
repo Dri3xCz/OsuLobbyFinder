51556b0e80deed051e46fb0e6bfad107aef554da
Dri3xCz <Dri3xCz@gmail.com>
2024-04-14 21:45:15 +0200

    init: LobbyFinder

---
+++ LobbyFinder.cs
@@ -0,0 +1,66 @@
+﻿using HtmlAgilityPack;
+using System;
+
+namespace OsuMultiplayerLobbyFinder
+{
+    public class LobbyFinder
+    {
+        public LobbyFinder() { }
+
+        public async Task<int> FindLobby(FindLobbyParameters parameters)
+        {
+            var httpClient = new HttpClient();
+            var htmlDocument = new HtmlDocument();
+            for (int i = 0; i < parameters.timeToLive; i++)
+            {
+                int matchId = parameters.startLobbyId - i;
+                try
+                {
+                    var html = await httpClient.GetStringAsync(
+                        $"https://osu.ppy.sh/community/matches/{matchId}"
+                    );
+                    htmlDocument.LoadHtml(html);
+
+                    string lobbyName = GetLobbyName(htmlDocument);
+
+                    if (MatchPattern(lobbyName, parameters.namePattern))
+                    {
+                        return matchId;
+                    }
+                } catch
+                {
+                    Console.WriteLine("Fetch failed! MatchId: " + matchId);
+                }
+
+                // Just for safety, the delay between fetches should be enough, but if this gets blocked, increase the delay
+                Thread.Sleep(500);
+            }
+
+            return 0;
+        }
+
+        private string GetLobbyName(HtmlDocument html)
+        {
+            return html.DocumentNode.SelectNodes("//title").First().InnerHtml;
+        }
+
+        private bool MatchPattern(string tested, string matcher)
+        {
+            return tested.Contains(matcher);
+        }
+    }
+
+    public struct FindLobbyParameters
+    {
+        public int startLobbyId;
+        public int timeToLive;
+        public string namePattern;
+
+        public FindLobbyParameters(int startLobbyId, int timeToLive, string namePattern)
+        {
+            this.startLobbyId = startLobbyId;
+            this.timeToLive = timeToLive;
+            this.namePattern = namePattern;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace OsuMultiplayerLobbyFinder;
+
+class Program
+{
+    public async static Task Main()
+    {
+        var LobbyFinder = new LobbyFinder();
+        Console.Write("ID of first lobby: ");
+        int id = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Time to live: ");
+        int ttl = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Name of lobby: ");
+        string name = Console.ReadLine();
+
+        FindLobbyParameters parameters = new FindLobbyParameters(
+            id,
+            ttl,
+            name
+        );
+
+        int result = await LobbyFinder.FindLobby(parameters);
+        Console.WriteLine("Succesfull find!: " + result);
+        // 0 znamená že to nic nenašlo
+    }
+}
