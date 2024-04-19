7ce96afae473803058ea95d3a897e68efa22a74c
Dri3xCz <Dri3xCz@gmail.com>
2024-04-19 22:13:48 +0200

    refactor: OsuApi apiKeyIsValid refactor

--- feature/OsuApi.cs
+++ feature/OsuApi.cs
@@ -8,26 +8,49 @@ namespace OsuMultiplayerLobbyFinder.feature
     public interface Api
     {
         public Task<bool> apiKeyIsValid(string apiKey);
+
+        public Task<LobbyModel> lobbyById(int id);
     }
 
     public class OsuApi : Api
     {
+        HttpClient client = new HttpClient();
+
         public async Task<bool> apiKeyIsValid(string apiKey)
         {
-            var client = new HttpClient();
             var uriBuilder = new UriBuilder("https://osu.ppy.sh/api/get_user_recent");
             uriBuilder.Query = $"k={apiKey}";
             var query = uriBuilder.ToString();
-     
-            try
-            {
-                string result = await client.GetStringAsync(query);
-            } catch
-            {
+
+            ResponseModel<object> response = await _GetAsync<object>(query);
+
+            if (response.Exception != null)
                 return false;
-            }
 
             return true;
+        }
+
+        public async Task<LobbyModel> lobbyById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        private async Task<ResponseModel<T>> _GetAsync<T>(string query)
+        {
+            try
+            {
+                string response = await client.GetStringAsync(query);
+                T? value = JsonSerializer.Deserialize<T>(response);
+
+                if (value == null)
+                    throw new JsonException();
+                
+                return new ResponseModel<T>(value);
+
+            } catch (Exception ex)
+            {
+                return new ResponseModel<T>(ex);
+            }
         }
     }
 }
---
+++ models/LobbyModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OsuMultiplayerLobbyFinder.models
+{
+    public class LobbyModel
+    {
+        public LobbyModel(Lobby lobby)
+        {
+            Lobby = lobby;
+        }
+
+        public Lobby Lobby { get; set; }
+    }
+
+    public class Lobby
+    {
+        public Lobby(int matchId, string name, DateTime startTime, DateTime endTime)
+        {
+            MatchId = matchId;
+            Name = name;
+            StartTime = startTime;
+            EndTime = endTime;
+        }
+
+        public int MatchId { get; set; }
+        public string Name { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+    }
+}
