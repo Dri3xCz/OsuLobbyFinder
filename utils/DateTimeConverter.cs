5893512385a6d867c6978b6a556c1eef119aa354
Dri3xCz <Dri3xCz@gmail.com>
2024-04-26 18:37:29 +0200

    feat: osuApi DateTimeConverter

---
+++ utils/DateTimeConverter.cs
@@ -0,0 +1,17 @@
+﻿namespace OsuMultiplayerLobbyFinder.utils
+{
+    public class DateTimeConverter
+    {
+        public DateTime OsuApiTimeToDateTime(string value)
+        {
+            int year = Convert.ToInt32(value.Substring(0, 4));
+            int month = Convert.ToInt32(value.Substring(5, 2));
+            int day = Convert.ToInt32(value.Substring(8, 2));
+            int hour = Convert.ToInt32(value.Substring(11, 2));
+            int minute = Convert.ToInt32(value.Substring(14, 2));
+            int second = Convert.ToInt32(value.Substring(17, 2));
+
+            return new DateTime(year, month, day, hour, minute, second);
+        }
+    }
+}
